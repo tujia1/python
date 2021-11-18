b4901e8d9ea0846c7d584c8990a678872e483b2a
tujia1 <1207960652@qq.com>
2021-11-18 17:10:25 +0800

    Add files via upload
    优化监控脚本，提供ip+端口随意组合

---
+++ export_new.py
@@ -0,0 +1,103 @@
+#!/bin/env python
+# -*- coding: utf8 -*-
+
+import socket
+import os
+import yaml
+import prometheus_client
+import requests
+from prometheus_client import Gauge
+from prometheus_client.core import CollectorRegistry
+from flask import Response, Flask
+app = Flask(__name__)
+
+def Fileconfig():
+    ospath = os.path.dirname(os.path.realpath(__file__))
+    yamlpath = os.path.join(ospath, "template.yaml")
+    with open(yamlpath, "r", encoding="utf-8") as f:
+        config = yaml.full_load(f)
+        return config
+
+
+def Urlexport(url):
+    r = requests.get(url, timeout=1)
+    time = r.elapsed.total_seconds()
+    result_de = {"url": url, "time": time}
+    return result_de
+
+
+def Exploreport(project, ip, port):
+    try:
+        connet = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        connet.connect((ip, int(port)))
+        connet.settimeout(1)
+        result_dic = {"project": project, "host": ip, "port": str(port), "status": 1}
+        return result_dic
+    except:
+        result_dic = {"project": project, "host": ip, "port": str(port), "status": 0}
+        return result_dic
+
+#def Checkport():
+#    data = Fileconfig()
+#    result_list = []
+#    for i in data.keys():
+#        if i != "http":
+#            iplist = data.get(i).get("host")
+#            ports = data.get(i).get("port")
+#            for ip in iplist:
+#                for port in ports:
+#                    result_dic = Exploreport(i, ip, port)
+#                    result_list.append(result_dic)
+#    return result_list
+
+def Checkport():
+    data = Fileconfig()
+    result_list = []
+    for i in data.keys():
+        if i != "http":
+            iplist = data.get(i).get("host")
+            for y in iplist:
+               try:
+                   ip = y.split(':')[0]
+                   port = y.split(':')[1]
+                   result_dic = Exploreport(i, ip, port)
+                   result_list.append(result_dic)
+               except:
+                   ports = data.get(i).get("port")
+                   for ip in iplist:
+                       for port in ports:
+                           result_dic = Exploreport(i, ip, port)
+                           result_list.append(result_dic)
+    return result_list
+
+def CheckUrl():
+    sul = Fileconfig()
+    result_list1 = []
+    for y in sul.keys():
+        if y == "http":
+            url1 = "".join(sul.get(y).get("url"))
+            result_dc = Urlexport(url1)
+            result_list1.append(result_dc)
+    return result_list1
+
+@app.route("/metrics")
+def ApiResponse():
+    checkport = Checkport()
+    checkurl = CheckUrl()
+    REGISTRY = CollectorRegistry(auto_describe=False)
+    mesStatus = Gauge("sszj_port_monitor", "api response stats is:", ["project", "ip", "port"], registry=REGISTRY)
+    mesurl = Gauge("sszj_port_url", "api response http time:", ["url"], registry=REGISTRY)
+    for datas in checkport:
+        project = "".join(datas.get("project"))
+        ip = "".join(datas.get("host"))
+        port = "".join(datas.get("port"))
+        status = datas.get("status")
+        for data1 in checkurl:
+            url = "".join(data1.get("url"))
+            time = data1.get("time")
+        mesStatus.labels(project, ip, port).set(status)
+        mesurl.labels(url).set(time)
+    return Response(prometheus_client.generate_latest(REGISTRY), mimetype="text/plain")
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5001", debug=True)
