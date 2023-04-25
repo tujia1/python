d5428229edb4369210fdf38d5775bb54be35cf9c
tujia1 <1207960652@qq.com>
2023-04-25 16:08:08 +0800

    Update export_new.py
    优化了端口检测数据模块

--- export_new.py
+++ export_new.py
@@ -10,7 +10,9 @@ from prometheus_client import Gauge
 from prometheus_client.core import CollectorRegistry
 from flask import Response, Flask
 app = Flask(__name__)
+REGISTRY = CollectorRegistry(auto_describe=False)
 
+# yaml文件解析
 def Fileconfig():
     ospath = os.path.dirname(os.path.realpath(__file__))
     yamlpath = os.path.join(ospath, "template.yaml")
@@ -18,14 +20,14 @@ def Fileconfig():
         config = yaml.full_load(f)
         return config
 
-
+# 网址检测
 def Urlexport(url):
     r = requests.get(url, timeout=1)
     time = r.elapsed.total_seconds()
     result_de = {"url": url, "time": time}
     return result_de
 
-
+#  端口检测
 def Exploreport(project, ip, port):
     try:
         connet = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -37,39 +39,26 @@ def Exploreport(project, ip, port):
         result_dic = {"project": project, "host": ip, "port": str(port), "status": 0}
         return result_dic
 
-#def Checkport():
-#    data = Fileconfig()
-#    result_list = []
-#    for i in data.keys():
-#        if i != "http":
-#            iplist = data.get(i).get("host")
-#            ports = data.get(i).get("port")
-#            for ip in iplist:
-#                for port in ports:
-#                    result_dic = Exploreport(i, ip, port)
-#                    result_list.append(result_dic)
-#    return result_list
-
+# 端口检测数据
 def Checkport():
     data = Fileconfig()
     result_list = []
     for i in data.keys():
         if i != "http":
             iplist = data.get(i).get("host")
-            for y in iplist:
+            ports = data.get(i).get("port")
+            for ip_port in iplist:
                try:
-                   ip = y.split(':')[0]
-                   port = y.split(':')[1]
+                   ip, port = ip_port.split(':')
                    result_dic = Exploreport(i, ip, port)
                    result_list.append(result_dic)
                except:
-                   ports = data.get(i).get("port")
-                   for ip in iplist:
                        for port in ports:
-                           result_dic = Exploreport(i, ip, port)
+                           result_dic = Exploreport(i, ip_port, port)
                            result_list.append(result_dic)
     return result_list
 
+# 网站监测数据
 def CheckUrl():
     sul = Fileconfig()
     result_list1 = []
@@ -80,11 +69,11 @@ def CheckUrl():
             result_list1.append(result_dc)
     return result_list1
 
+# flask_api 展示数据prometheus_clinet转换为Gauge
 @app.route("/metrics")
 def ApiResponse():
     checkport = Checkport()
     checkurl = CheckUrl()
-    REGISTRY = CollectorRegistry(auto_describe=False)
     mesStatus = Gauge("sszj_port_monitor", "api response stats is:", ["project", "ip", "port"], registry=REGISTRY)
     mesurl = Gauge("sszj_port_url", "api response http time:", ["url"], registry=REGISTRY)
     for datas in checkport:
